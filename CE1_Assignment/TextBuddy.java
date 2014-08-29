af6987cb01b36ed20f5bb2441f7eb4e95db16ee6
Malavika <malavika94@yahoo.com.sg>
2014-08-29 13:56:10 +0800

    Almost done

--- CE1_Assignment/TextBuddy.java
+++ CE1_Assignment/TextBuddy.java
@@ -11,7 +11,7 @@ public class TextBuddy {
 	private static final String MESSAGE_EMPTY_DATA = "\"%1$s\" is empty.";
 	private static final String MESSAGE_DELETE = "Deleted \"%1$s\"";
 	private static final String MESSAGE_COMMAND = "command: ";
-	private static final String MESSAGE_NO_SPACE = "No more space to store"; 
+	//private static final String MESSAGE_NO_SPACE = "No more space to store"; 
 	//Maximum of 10 strings can be save only in the array using this textbuddy
 	
 	private static final String ERROR_INVALID_COMMAND = "Error: Invalid command. Command \"%1$s\" does not exist";
@@ -27,7 +27,7 @@ public class TextBuddy {
 	
 	private static final int PARAM_START = 0;
 	private static final int NUM_TEXT = 10;
-	private static final String[] textline = new String[NUM_TEXT];
+	private static final String[] textList = new String[NUM_TEXT];
 	
 	private static Scanner sc = new Scanner(System.in);
 	
@@ -43,9 +43,11 @@ public class TextBuddy {
 	private static void showToUser(String text){
 		System.out.print(text);
 	}
+	
 	private static void exit() {
 		System.exit(0);
 	}
+	
 	public static String executeCommand(String userCommand){
 		if (userCommand.trim().equals("")){
 			return String.format(MESSAGE_INVALID_COMMAND, userCommand);
@@ -99,11 +101,43 @@ public class TextBuddy {
 	
 	private static String addText(String userCommand){
 		String textToBeAdded = removeFirstWord(userCommand);
-		
+		int i = 0;
+		while (i<NUM_TEXT){
+			if (textList[i] != null){
+				i++;
+			}
+			else {
+				textList[i] = textToBeAdded;
+				return String.format(MESSAGE_ADD, textToBeAdded);
+			}
+		}
+		return String.format(ERROR_CANT_ADD, textToBeAdded);
 	}
+	
+	private static String removeText (String userCommand) {
+		String textToBeRemoved = removeFirstWord(userCommand);
+		int i = 0;
+		while (i<NUM_TEXT){
+			if (textToBeRemoved.equals(textList[i])){
+				textList[i] = null;
+			}
+			else {
+				return String.format(ERROR_DOESNT_EXIST, textToBeRemoved);
+			}
+		}
+	}
+	
+	private static String clearAllText (String userCommand){
+		String clearAll = removeFirstWord(userCommand);
+		for (int i=0; i<NUM_TEXT; i++) {
+			textList[i] = null;
+		}
+	}
+	
 	private static String removeFirstWord(String userCommand) {
 		return userCommand.replace(getFirstWord(userCommand), "").trim();
 	}
+	
 	private static String getFirstWord(String userCommand) {
 		String commandTypeString = userCommand.trim().split("\\s+")[0];
 		return commandTypeString;
